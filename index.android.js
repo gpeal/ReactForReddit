1d2455cfec2f4888e0de544ea67ed48631855605
Gabriel Peal <gabriel.peal@airbnb.com>
2016-05-01 22:04:10 -0700

    Made the story card prettier

--- index.android.js
+++ index.android.js
@@ -22,7 +22,7 @@ class ReactForReddit extends Component {
         this.state = {
             movies: null,
             dataSource: new ListView.DataSource({
-                rowHasChanged: (row1, row2) => row1.data.name !== row2.data.name
+                rowHasChanged: (row1, row2) => row1.data.id !== row2.data.id
             }),
             loaded: false
         };
@@ -70,12 +70,24 @@ class ReactForReddit extends Component {
     renderItem(item) {
         const story = item.data;
         return (
-            <View key={story.name} style={styles.container}>
-                <PreviewImage preview={story.preview}/>
-                <View style={styles.rightContainer}>
+            <View key={story.name} style={styles.storyContainer}>
+                <View style={styles.contentContainer}>
+                    <View style={styles.topContainer}>
+                        <Text style={styles.votes}>{story.score}</Text>
+                        <Text style={styles.authorAndSubreddit}>{story.author}</Text>
+                        <Text style={styles.in}> in </Text>
+                        <Text style={styles.authorAndSubreddit}>{story.subreddit}</Text>
+                    </View>
                     <Text style={styles.title}>{story.title}</Text>
-                    <Text style={styles.author}>{story.author}</Text>
+                    <View style={styles.bottomContainer}>
+                        <Text style={styles.bottomText}>
+                            {story.num_comments} Comments
+                            {' • '}
+                            {story.domain}
+                        </Text>
+                   </View>
                 </View>
+                <PreviewImage preview={story.preview}/>
             </View>
         );
     }
@@ -96,31 +108,67 @@ class PreviewImage extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
+    storyContainer: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: 'white',
+        marginLeft: 8,
+        marginTop: 4,
+        marginRight: 8,
+        marginBottom: 4,
+        paddingLeft: 8,
+        paddingTop: 4,
+        paddingRight: 8,
+        paddingBottom: 4,
+        elevation: 4
+    },
+    topContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    bottomContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
     },
     thumbnail: {
         width: 53,
         height: 81
     },
-    rightContainer: {
+    contentContainer: {
         flex: 1
     },
     title: {
         fontSize: 14,
+        marginTop: 6,
         marginBottom: 8,
-        textAlign: 'center'
+        color: '#444444',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        flex: 1
     },
     author: {
         textAlign: 'center'
     },
     listView: {
         paddingTop: 20,
-        backgroundColor: '#F5FCFF'
+        backgroundColor: '#EEEEEE'
+    },
+    votes: {
+        fontSize: 13,
+        color: "#AAAAAA",
+        paddingRight: 4
+    },
+    authorAndSubreddit: {
+        fontSize: 11,
+        color: '#0E3D4D'
+    },
+    in: {
+        fontSize: 10,
+        color: '#858585'
+    },
+    bottomText: {
+        fontSize: 10,
+        color: '#555555'
     }
 });
 
